8c0e378bc78a89f571ea5065f9972303504c80ad
Oleg Dianov <odidoit@gmail.com>
2018-07-09 17:42:02 +0300

    Create docker-entrypoint.sh

---
+++ redis-sentinel/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+REDIS_PARAMS=(
+  REDIS_SENTINEL_PORT
+  REDIS_SENTINEL_QUORUM
+  REDIS_SENTINEL_DOWN_AFTER
+  REDIS_SENTINEL_FAILOVER_TIMEOUT
+  REDIS_MASTER_GROUP
+  REDIS_MASTER_IP
+  REDIS_MASTER_PORT
+)
+
+for param in "${REDIS_PARAMS[@]}"; do
+  param_value="$(eval printf %s \"\$$param\")"
+  sed -i "s|\$$param|$param_value|g" "$REDIS_SENTINEL_CONF_PATH"
+done
+
+exec redis-server "$REDIS_SENTINEL_CONF_PATH" --sentinel "$@"
